2d8a0550c7bde47f8371edea89e3b3048e526a9e
Armando Blancas <abm221617@gmail.com>
2013-03-18 10:15:10 -0700

    Updated lein project, dep versions.

--- project.clj
+++ project.clj
@@ -3,8 +3,15 @@
   :license {:name "Eclipse Public License"
 	    :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/blancas/morph"
-  :plugins [[codox "0.6.4"]]
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :source-paths ["src/main/clojure" "src/main/resources"]
-  :codox {:sources ["src/main/clojure"] :output-dir "codox"}
-  :jvm-opts ["-Dfile.encoding=UTF-8"])
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
+  :jvm-opts ["-Dfile.encoding=UTF-8"]
+  :profiles
+    {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+     :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+     :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+     :dev {:resource-paths ["src/main/resources" "src/test/resources"]
+           :dependencies [[midje "1.5.0" :exclusions [org.clojure/clojure]]]
+           :plugins [[codox "0.6.4"]]
+           :codox {:sources ["src/main/clojure"] :output-dir "codox"}}})
