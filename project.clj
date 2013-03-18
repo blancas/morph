78877918a108d598290ae44879b6b5354ef68a71
Armando Blancas <abm221617@gmail.com>
2013-03-17 22:10:54 -0700

    Bumped up versions.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.blancas/morph "0.1.0"
+(defproject org.blancas/morph "0.2.0"
   :description "A Library of Morphisms: Monoids, Functors, and Monads"
   :license {:name "Eclipse Public License"
 	    :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/blancas/morph"
   :plugins [[codox "0.6.4"]]
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :source-paths ["src/main/clojure" "src/main/resources"]
   :codox {:sources ["src/main/clojure"] :output-dir "codox"}
   :jvm-opts ["-Dfile.encoding=UTF-8"])
